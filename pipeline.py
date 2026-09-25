bdf38ee63108ce05692d1f25dcbf4c36c6b1ad4e
Jaswanthi Vutukuri <jaswanthivu@gmail.com>
2026-09-25 18:17:10 -0400

    implemented command-line argument parsing

--- pipeline.py
+++ pipeline.py
@@ -34,7 +34,37 @@ def setup_logging(verbose=False):
 
 def parse_arguments():
     """Parse command-line arguments."""
-    pass  # TODO: implement
+   
+    parser = argparse.ArgumentParser(
+        description="Run the data processing pipeline"
+    )
+
+    parser.add_argument(
+        "--input", "-i",
+        required=True,
+        help="Path to input file"
+    )
+
+    parser.add_argument(
+        "--output", "-o",
+        required=True,
+        help="Path to output file"
+    )
+
+    parser.add_argument(
+        "--format",
+        choices=["csv", "json"],
+        default="csv",
+        help="Output format"
+    )
+
+    parser.add_argument(
+        "--verbose", "-v",
+        action="store_true",
+        help="Show the detailed DEBUG messages"
+    )
+
+    return parser.parse_args()
 
 
 def validate_input(filepath):
